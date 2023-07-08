2bc13fd8b57a8db1733801a2441097af66dd847b
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-08 13:13:34 +0300

    added second task

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 5; b = 25 -> да
+// a = 2 b = 10 -> нет
+// a = 9; b = -3 -> нет
+// a = -3 b = 9 -> да
+// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+
+Console.WriteLine("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является кадратом второго");
+}
